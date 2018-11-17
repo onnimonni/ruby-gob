a0a2c6b282e267dd863c14726c0252a45da77a8b
Onni Hakala <onni@keksi.io>
2018-11-17 19:05:31 +0200

    Implement boolean type for decode

--- lib/gob/utils/decoder.rb
+++ lib/gob/utils/decoder.rb
@@ -5,7 +5,7 @@ class Gob::Utils::Decoder
 
 	# Calculates the content length according to the first bytes
 	# Source: https://golang.org/pkg/encoding/gob/
-	def content_byte_length
+	def content_byte_length(content=@content)
 		first_byte = @content[0].unpack('C')[0]
 		if first_byte <= 128
 			# First byte tells the length if byte count is smaller than 128
@@ -25,8 +25,50 @@ class Gob::Utils::Decoder
 
 	# Checks if the gob encoded content length is valid and
 	# no bytes were missing in the transit
-	def content_length_correct?
-		length, check_bytes = content_byte_length
-		length == @content.length - check_bytes
+	def content_length_correct?(content=@content)
+		length, check_bytes = content_byte_length(content)
+		length == content.length - check_bytes
+	end
+
+	# These are the supported types for gob encoding
+	TYPES = {
+		1 => :bool,
+		2 => :int,
+		3 => :uint,
+		4 => :float,
+		5 => :byte_array,
+		6 => :string,
+		7 => :complex,
+		8 => :interface,
+		# gap for reserved ids.
+		16 => :WireType,
+		17 => :ArrayType,
+		18 => :CommonType,
+		19 => :SliceType,
+		20 => :StructType,
+		21 => :FieldType,
+		22 => :FieldType_slice,
+		23 => :MapType
+	}.freeze
+
+	# Checks which kind of data is included
+	def type(content=@content)
+		content_length, skip_bytes = content_byte_length(@content)
+		type_byte = @content[skip_bytes].unpack('C')[0]/2
+		type = TYPES[type_byte]
+		unless type
+			raise NotImplementedError, "Type #{type_byte} is not yet implemented in ruby-gob"
+		end
+		type
+	end
+
+	def decode(content=@content)
+		@type ||= type(content)
+		case @type
+		when :bool
+			@content[-1].unpack('C')[0] == 1
+		else
+			raise NotImplementedError, "Type #{@type} is not yet implemented in ruby-gob"
+		end
 	end
 end
---
+++ spec/gob/utils/decoder/boolean_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe Gob::Utils::Decoder do
+  context "boolean" do
+    context "true" do
+
+      let!(:content) { "\x03\x02\x00\x01" }
+      let!(:decoder) { Gob::Utils::Decoder.new(content) }
+
+      it "has correct length according to bytes" do
+        expect(decoder.content_byte_length).to eq [3,1]
+      end
+
+      it "has correct length" do
+        expect(decoder.content_length_correct?).to be true
+      end
+
+      it "has correct type" do
+        expect(decoder.type).to be :bool
+      end
+
+      it "converts to true" do
+        expect(decoder.decode).to be true
+      end
+    end
+
+    context "false" do
+      let!(:content) { "\x03\x02\x00\x00" }
+      let!(:decoder) { Gob::Utils::Decoder.new(content) }
+
+      it "has correct length according to bytes" do
+        expect(decoder.content_byte_length).to eq [3,1]
+      end
+
+      it "has correct length" do
+        expect(decoder.content_length_correct?).to be true
+      end
+
+      it "has correct type" do
+        expect(decoder.type).to be :bool
+      end
+
+      it "converts to false" do
+        expect(decoder.decode).to be false
+      end
+    end
+  end
+end
